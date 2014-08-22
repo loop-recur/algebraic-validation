19af2007fee34f1269633b316531dd504198d406
Adam C. Baker <adam.durandal@gmail.com>
2014-08-21 18:27:33 -0700

    extracted implementation.

---
+++ validation.js
@@ -0,0 +1,49 @@
+function Validation() {};
+
+function Success(val) {
+  if(this instanceof Success){
+    this.value = val;
+  } else {
+    return new Success(val);
+  }
+}
+Success.prototype = new Validation();
+Success.prototype.is_success = true;
+Success.prototype.is_failure = false;
+Success.prototype.map = function(f) {
+  return new Success(f(this.value));
+}
+Success.prototype.chain = function(f){
+  return f(this.value);
+}
+Success.prototype.join = function(){ return this.value; }
+Success.prototype.concat = function(other){ return other; };
+Success.prototype.traverse = function(f, pure) {
+  return f(this.value).map(Success);
+};
+
+//Failure's argument must be a semigroup (have a valid concat function)
+function Failure(val) {
+  if(this instanceof Failure){
+    this.value = val;
+  } else {
+    return new Failure(val);
+  }
+}
+Failure.prototype = new Validation();
+Failure.prototype.is_success = false;
+Failure.prototype.is_failure = true;
+Failure.prototype.map = function(f){ return this; }
+Failure.prototype.chain = function(f){ return this; }
+Failure.prototype.join = function(f){ return this; }
+Failure.prototype.concat = function(other){
+  return other.is_success ? this :
+    new Failure(this.value.concat(other.value));
+};
+Failure.prototype.traverse = function(f, pure) { return pure(this); };
+
+Validation.prototype.of = Validation.of = Failure.of = Success.of = Success;
+Validation.Success = Success;
+Validation.Failure = Failure;
+
+module.exports = Validation;
